bc4d33ab7ffd32b45e0d6bdff285721a85489758
szlonik <jakubbednarz31@gmail.com>
2023-11-08 12:45:09 +0100

    dodanie zawartosci plikow podstron

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function redirectToHomePage() {
     window.location.href = "index.html";
 }
-
+function redirectToAboutPage() {
+    window.location.href = "o_nas.html";
+}
+function redirectToMyProfilePage() {
+    window.location.href = "moje_konto.html";
+}
+function redirectToSupportPage() {
+    window.location.href = "wsparcie.html";
+}
 document.addEventListener("DOMContentLoaded", function () {
     var logoElements = document.getElementsByClassName("logo");
 
     for (var i = 0; i < logoElements.length; i++) {
         logoElements[i].addEventListener("click", redirectToHomePage);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var logoElements = document.getElementsByClassName("material-icons-sharp");
+
+    for (var i = 0; i < logoElements.length; i++) {
+        logoElements[i].addEventListener("click", redirectToMyProfilePage);
     }
 });
